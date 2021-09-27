4a3e1cb81ab3e9fe293369d2d10d4f7b53cf91c5
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-09-27 15:18:47 -0700

    form for measurement and some CSS

--- src/Weather.js
+++ src/Weather.js
@@ -3,17 +3,27 @@ import './Weather.css'
 
 function Weather () {
     const [zip, setZip] = useState('94107')
+    const [unit, setUnit] = useState(' ')
 
     return (
         <div className="Weather">
-            <h1>{zip}</h1>
+            <h1>{zip} {unit}</h1>
+
             <form>
-                <input 
-                    placeHolder="Enter zip code"
-                    value={zip}
-                    onChange={ e => setZip(e.target.value) }
-                />
-                <button>Submit</button>
+                <div>
+                    <input 
+                        placeHolder="Enter zip code"
+                        value={zip}
+                        onChange={ e => setZip(e.target.value) }
+                    />
+                    <button>Submit</button>
+                </div>
+
+                <select onChange={e => setUnit(e.target.value)}>
+                    <option value="metric">Celcius</option>
+                    <option value="imperial">Farenheit</option>
+                    <option value="standard">Kelvin</option>
+                </select>
             </form>
         </div>
     )

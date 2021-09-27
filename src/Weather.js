2e69b4ba5abccb1e9a34858f5ed2149f82e0fcbf
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-09-27 15:09:39 -0700

    implemented useState

--- src/Weather.js
+++ src/Weather.js
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import './Weather.css'
 
 function Weather () {
+    const [zip, setZip] = useState('94107')
+
     return (
         <div className="Weather">
+            <h1>{zip}</h1>
             <form>
                 <input 
                     placeHolder="Enter zip code"
+                    value={zip}
+                    onChange={ e => setZip(e.target.value) }
                 />
                 <button>Submit</button>
             </form>
